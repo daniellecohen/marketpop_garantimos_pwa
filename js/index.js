2b2ad9216bb4ded29baec748613d5c0230e22fc2
Dayvid Montovanelli Moreira <dayvidmoreira7@gmail.com>
2019-06-14 20:01:18 -0300

    tageando hotjar

--- js/index.js
+++ js/index.js
@@ -4,6 +4,15 @@ gtag('js', new Date());
 
 gtag('config', 'UA-142154778-1');
 
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:1364515,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+
 var url = 'https://marketpop-garantimos-api.herokuapp.com' // 'http://localhost:10'
 
 if(window.location.pathname.split('/').pop() == 'main.html') {
